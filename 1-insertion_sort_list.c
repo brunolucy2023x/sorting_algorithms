8951da63db885267e3659243ce614cee4dd8824a
brunolucy2023x <lucyb5147@gmail.com>
2024-02-20 00:56:41 -0800

    34

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,14 +1,15 @@
 #include "sort.h"
-/*
-* 88 88b 88 .dP"Y8 888888 88""Yb 888888 88  dP"Yb  88b 88
-* 88 88Yb88 `Ybo." 88__   88__dP   88   88 dP   Yb 88Yb88
-* 88 88 Y88 o.`Y8b 88""   88"Yb    88   88 Yb   dP 88 Y88
-* 88 88  Y8 8bodP' 888888 88  Yb   88   88  YbodP  88  Y8
-*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 void move_left(listint_t *curr, listint_t *insertion, listint_t **head);
 /**
  * insertion_sort_list - sort a doubly linked list of integer
  * in ascending order
+ * name - Bruno  Owino
+ * email - brunookoth44@gmail.com
  * @list: pointer to the head of the doubly linked list
  */
 
