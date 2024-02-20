4efbdda5d9c3a8d27320dc39a779bb0633cc89f8
brunolucy2023x <lucyb5147@gmail.com>
2024-02-19 22:44:15 -0800

    THIRD

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,17 +1,17 @@
 #include "sort.h"
 
 /**
- * swap_ints - Swap two integers in an array.
- * @aa: The first integer to swap.
- * @bb: The second integer to swap.
+ * swap - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
  */
-void swap_ints(int *aa, int *bb)
+void swap(int *a, int *b)
 {
-	int temp;
+	int tmp;
 
-	temp = *aa;
-	*aa = *bb;
-	*bb = temp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
 /**
@@ -23,7 +23,7 @@ void swap_ints(int *aa, int *bb)
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, leen = size;
+	size_t i, len = size;
 	bool bubbly = false;
 
 	if (array == NULL || size < 2)
@@ -32,15 +32,15 @@ void bubble_sort(int *array, size_t size)
 	while (bubbly == false)
 	{
 		bubbly = true;
-		for (i = 0; i < leen - 1; i++)
+		for (i = 0; i < len - 1; i++)
 		{
 			if (array[i] > array[i + 1])
 			{
-				swap_ints(array + i, array + i + 1);
+				swap(array + i, array + i + 1);
 				print_array(array, size);
 				bubbly = false;
 			}
 		}
-		leen--;
+		len--;
 	}
 }
